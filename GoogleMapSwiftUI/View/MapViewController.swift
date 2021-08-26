3f80e35faaae4ae1f4ca4b3990848969b4dadd6f
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-08-26 17:17:26 +0600

    Apply custom style using .json

--- GoogleMapSwiftUI/View/MapViewController.swift
+++ GoogleMapSwiftUI/View/MapViewController.swift
@@ -20,16 +20,42 @@
 //
 
 import GoogleMaps
+import GoogleMapsUtils
 import SwiftUI
 import UIKit
 
 class MapViewController: UIViewController {
     
+    let camera =  GMSCameraPosition.camera(withLatitude: 24.78504812901799, longitude:  90.39632986635996, zoom: 14.5)
     let map =  GMSMapView(frame: .zero)
+    //    var clusterManager: GMUClusterManager!
+    
     var isAnimating: Bool = false
+    
+    
+    override func viewDidLoad() {
+        let iconGenerator = GMUDefaultClusterIconGenerator()
+        let algorithm = GMUNonHierarchicalDistanceBasedAlgorithm()
+        let renderer = GMUDefaultClusterRenderer(mapView: map,
+                                                 clusterIconGenerator: iconGenerator)
+        //        clusterManager = GMUClusterManager(map: map, algorithm: algorithm,
+        //                                                  renderer: renderer)
+    }
     
     override func loadView() {
         super.loadView()
+        map.camera = camera
+        do {
+            // Set the map style by passing the URL of the local file.
+            if let styleURL = Bundle.main.url(forResource: "CustomMapStyle", withExtension: "json") {
+                map.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+                print("Style applied")
+            } else {
+                print(".json file not found")
+            }
+        } catch {
+            print("One or more of the map styles failed to load. \(error)")
+        }
         self.view = map
     }
 }
